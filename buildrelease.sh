8c3b2c760c89807d84caa6fabda0316249b275a3
madortill <mador.till@gmail.com>
2021-11-02 10:44:24 +0200

    update

--- buildrelease.sh
+++ buildrelease.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-APK_PATH="./platforms/android/build/outputs/apk/"
-APK_UNSIGNED_NAME="android-release-unsigned.apk"
-KEYSTORE_PATH="E:/Code/AndroidKeystores/driveapp.keystore"
-FINISHED_APK_PATH="E:/Code/Production/driveapp.apk"
+APK_PATH="D:/projects/under_development/app_ionic-driveapp/platforms/android/app/build/outputs/bundle/release/"
+APK_UNSIGNED_NAME="app-release.aab"
+KEYSTORE_PATH="E:/Code/AndroidKeystores/driveapp8.keystore"
+FINISHED_APK_PATH="E:/Code/Production/driveapp.aab"
 
 echo -e "Enter alias: \c"
 read android_alias
-ionic cordova build android --release
+ionic cordova build android --release -- -- --packageType=bundle
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE_PATH $APK_PATH$APK_UNSIGNED_NAME $android_alias
 rm $FINISHED_APK_PATH
 zipalign.exe -v 4 $APK_PATH$APK_UNSIGNED_NAME $FINISHED_APK_PATH
+read -p "Press enter to continue"
